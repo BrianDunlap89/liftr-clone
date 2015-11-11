dff886abcd490715f381a80caf86c1af577f5604
Brian Dunlap <briandunlap589@gmail.com>
2015-11-10 19:47:45 -0500

    Add basic tests for User and Post models

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_validate_required_params_to_create
+    post = Post.new(title: "Test", link: "http://www.google.com", user_id: 4)
+    assert post.save
+    post = Post.new(title: "Test", user_id: 4)
+    refute post.save
+    post = Post.new(link: "http://www.google.com", user_id: 4)
+    refute post.save
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  
+  def test_validate_required_params_to_create_user
+    user = User.new(username: "test", email: "name@example.com", password: "tomatoes")
+    assert user.save
   end
+
+  def test_validate_params_required_for_new_user
+    user = User.new(username: "name", password: "tomatoes")
+    refute user.save
+    user = User.new(email: "test@example.com", password: "tomatoes")
+    refute user.save
+  end
+  
 end
